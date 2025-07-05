554f4c989d6accb2ebee472e007515c7223cde52
Sonu Kumar <sonuhits047@gmail.com>
2025-07-06 01:28:00 +0530

    Updated portfolio latest

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+// let header = document.querySelector("header");
+// let menu = document.querySelector("#menu-icon");
+// let navbar = document.querySelector("#nav-menu");
+
+// window.addEventListener("scroll", () => {
+//   header.classList.toggle("shadow", window.scrollY > 0);
+// });
+
+// menu.onclick = () => {
+//   navbar.classList.toggle("active");
+// };
+// window.onscroll = () => {
+//   navbar.classList.remove("active");
+// };
+
+// // Dark Mode / light mode
+// let darkmode = document.querySelector("#darkmode");
+
+// darkmode.onclick = () => {
+//   if (darkmode.classList.contains("bx-moon")) {
+//     darkmode.classList.replace("bx-moon", "bx-sun");
+//     document.body.classList.add("active");
+//   } else {
+//     darkmode.classList.replace("bx-sun", "bx-moon");
+//     document.body.classList.remove("active");
+//   }
+// };
+
+      
+
+// document.getElementById('resume-link-1').addEventListener("click", () => {
+//   console.log("Script file loaded successfully");
+//   window.open("https://drive.google.com/file/d/1VXxIiJAIwC9OXHfo8KjqkTRox1v8_Y_t/view?usp=drive_link", "_blank");
+// });
+
+// document.getElementById('resume-link-2').addEventListener("click", () => {
+//   console.log("Script file loaded successfully");
+//   window.open("https://drive.google.com/file/d/1VXxIiJAIwC9OXHfo8KjqkTRox1v8_Y_t/view?usp=drive_link", "_blank");
+// });
+
+
+
+//AI
+
+// script.js
 let header = document.querySelector("header");
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector("#nav-menu");
@@ -9,6 +54,7 @@ window.addEventListener("scroll", () => {
 menu.onclick = () => {
   navbar.classList.toggle("active");
 };
+
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
@@ -26,14 +72,9 @@ darkmode.onclick = () => {
   }
 };
 
-      
+// Resume button event
+const resumeLink = "https://drive.google.com/file/d/1VXxIiJAIwC9OXHfo8KjqkTRox1v8_Y_t/view?usp=drive_link";
 
 document.getElementById('resume-link-1').addEventListener("click", () => {
-  console.log("Script file loaded successfully");
-  window.open("https://drive.google.com/file/d/1VXxIiJAIwC9OXHfo8KjqkTRox1v8_Y_t/view?usp=drive_link", "_blank");
-});
-
-document.getElementById('resume-link-2').addEventListener("click", () => {
-  console.log("Script file loaded successfully");
-  window.open("https://drive.google.com/file/d/1VXxIiJAIwC9OXHfo8KjqkTRox1v8_Y_t/view?usp=drive_link", "_blank");
+  window.open(resumeLink, "_blank");
 });
